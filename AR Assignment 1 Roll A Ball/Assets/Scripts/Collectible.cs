7cddcd802247de2deb72d757e68977f36021312f
Yiyang Zeng <yz3622@nyu.edu>
2020-02-08 17:40:40 -0500

    Redesign collectibles

--- AR Assignment 1 Roll A Ball/Assets/Scripts/Collectible.cs
+++ AR Assignment 1 Roll A Ball/Assets/Scripts/Collectible.cs
@@ -13,6 +13,6 @@ public class Collectible : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
     }
 }
