455eda02015c0c51d124267835c505a87bb16636
Yiyang Zeng <yz3622@nyu.edu>
2020-02-08 16:36:23 -0500

    Add the funtionality to switch between cameras

--- AR Assignment 1 Roll A Ball/Assets/Scripts/GameState.cs
+++ AR Assignment 1 Roll A Ball/Assets/Scripts/GameState.cs
@@ -7,11 +7,17 @@ public class GameState : MonoBehaviour
 {
     public int score = 0;
     public Text scoreText;
+    public Camera main;
+    public Camera map;
+
+    private Camera current;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreText.text = "Score: 0";
+        current = main;
+        map.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,5 +29,21 @@ public class GameState : MonoBehaviour
     public void UpdateScore()
     {
         scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void SwitchCamera()
+    {
+        if(current == main)
+        {
+            current = map;
+            main.gameObject.SetActive(false);
+            map.gameObject.SetActive(true);
+        }
+        else
+        {
+            current = main;
+            map.gameObject.SetActive(false);
+            main.gameObject.SetActive(true);
+        }
     }
 }
