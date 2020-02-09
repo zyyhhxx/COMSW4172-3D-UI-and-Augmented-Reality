9b84842cc285ecfa57b3c75b5a3042e6a915f8c8
Yiyang Zeng <yz3622@nyu.edu>
2020-02-09 17:59:05 -0500

    Finish building the resizing object

---
+++ AR Assignment 1 Roll A Ball/Assets/Scripts/Resizing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizing : MonoBehaviour
+{
+    public float rate = 0.5f;
+    public float upperLimit = 2;
+    public float lowerLimit = 0.5f;
+    private bool grow = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newScale;
+        if (grow)
+            newScale = transform.localScale.x + rate * Time.deltaTime;
+        else
+            newScale = transform.localScale.x - rate * Time.deltaTime;
+        if (newScale > upperLimit || newScale < lowerLimit)
+            grow = !grow;
+        transform.localScale = new Vector3(newScale, transform.localScale.y, transform.localScale.z);
+    }
+}
