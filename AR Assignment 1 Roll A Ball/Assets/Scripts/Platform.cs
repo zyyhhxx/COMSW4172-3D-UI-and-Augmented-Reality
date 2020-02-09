b98a8f6d0230cdb2be2ec5476ef49da6aea8bd6f
Yiyang Zeng <yz3622@nyu.edu>
2020-02-09 14:35:59 -0500

    Add the code to change spawn point

---
+++ AR Assignment 1 Roll A Ball/Assets/Scripts/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameState state;
+    public GameObject spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            state.spawnPoint = spawnPoint;
+        }
+    }
+}
