01ce2e072fde78e52d60a7391004af49309acca9
Yiyang Zeng <yz3622@nyu.edu>
2020-04-17 14:37:41 -0400

    Implement the firing of the enemy base

--- AR Assignment 3 TowAR Defense/Assets/Scripts/EnemyBase.cs
+++ AR Assignment 3 TowAR Defense/Assets/Scripts/EnemyBase.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    public int respawnDelay = 1;
-    public int spawnDelay = 10;
+    public int respawnDelay = 5;
+    public int spawnDelay = 20;
+    public int fireRate = 10;
+    public float force = 0.3f;
     public GameManager gm;
     public GameObject enemy;
+    public GameObject ammo;
+    public GameObject firePoint;
+    public GameObject[] targets;
 
     // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
         StartCoroutine(SpawnEnemy());
+        StartCoroutine(FindTarget());
     }
 
     // Update is called once per frame
@@ -33,6 +39,30 @@ public class EnemyBase : MonoBehaviour
         {
             GameObject.Instantiate(enemy, this.gameObject.transform.position, this.gameObject.transform.rotation, GameObject.Find("Base Plane").transform);
             yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    IEnumerator FindTarget()
+    {
+        for (;;)
+        {
+            // Get the target
+            targets = GameObject.FindGameObjectsWithTag("Tower");
+            if (targets.Length > 0)
+            {
+                int index = Random.Range(0, targets.Length);
+                var target = targets[index];
+
+                //Fire at the target
+                var heading = target.transform.position - this.gameObject.transform.position;
+                var distance = heading.magnitude;
+                var direction = heading / distance;
+                var fireDirection = new Vector3(direction.x, 1, direction.z);
+                Debug.Log((heading, distance, fireDirection));
+                var firedAmmo = GameObject.Instantiate(ammo, firePoint.transform.position, firePoint.transform.rotation, GameObject.Find("Base Plane").transform);
+                firedAmmo.GetComponent<Rigidbody>().AddForce(fireDirection * Random.Range(force * 0.5f, force), ForceMode.Impulse);
+            }
+            yield return new WaitForSeconds(fireRate);
         }
     }
 }
