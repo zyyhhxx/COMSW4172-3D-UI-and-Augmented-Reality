fd263879b0d89aee0343d58b7ab629ba48e8a2dd
Yiyang Zeng <yz3622@nyu.edu>
2020-04-16 22:08:52 -0400

    Implement enemy spawning

---
+++ AR Assignment 3 TowAR Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
--- AR Assignment 3 TowAR Defense/Assets/Scripts/EnemyBase.cs
+++ AR Assignment 3 TowAR Defense/Assets/Scripts/EnemyBase.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
     public int respawnDelay = 1;
+    public int spawnDelay = 10;
     public GameManager gm;
+    public GameObject enemy;
 
     // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        StartCoroutine(SpawnEnemy());
     }
 
     // Update is called once per frame
@@ -24,5 +27,12 @@ public class EnemyBase : MonoBehaviour
         gm.Respawn(this.gameObject.transform.position, this.gameObject.transform.rotation, respawnDelay);
     }
 
-    
+    IEnumerator SpawnEnemy()
+    {
+        for(;;)
+        {
+            GameObject.Instantiate(enemy, this.gameObject.transform.position, this.gameObject.transform.rotation, GameObject.Find("Base Plane").transform);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
 }
