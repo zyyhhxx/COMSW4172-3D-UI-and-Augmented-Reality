8d60c8d65ddcb66c191627e15fb4eef5a374f18e
Yiyang Zeng <yz3622@nyu.edu>
2020-04-16 23:40:42 -0400

    Implement health bar

--- AR Assignment 3 TowAR Defense/Assets/Scripts/Health.cs
+++ AR Assignment 3 TowAR Defense/Assets/Scripts/Health.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public int health;
-    public int maxHealth = 100;
+    public float health;
+    public float maxHealth = 100;
+    public Slider slider;
+    public Image fill;
 
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        slider = this.gameObject.GetComponentInChildren<Slider>();
+        fill = slider.GetComponentInChildren<Image>();
+        slider.maxValue = maxHealth;
+        slider.value = health;
+        fill.color = Color.green;
     }
 
     // Update is called once per frame
@@ -18,5 +26,10 @@ public class Health : MonoBehaviour
     {
         if (health <= 0)
             Destroy(this.gameObject);
+        else
+        {
+            slider.value = health;
+            fill.color = new Color(1-health / maxHealth, health/maxHealth, 0, 1);
+        }
     }
 }
