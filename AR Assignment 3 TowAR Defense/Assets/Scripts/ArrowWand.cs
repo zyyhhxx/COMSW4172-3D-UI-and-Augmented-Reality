a188f016361634127d8aab6e7b21e8675bdb9ba5
Yiyang Zeng <yz3622@nyu.edu>
2020-04-17 22:19:10 -0400

    Implement arrow wand raycasting

--- AR Assignment 3 TowAR Defense/Assets/Scripts/ArrowWand.cs
+++ AR Assignment 3 TowAR Defense/Assets/Scripts/ArrowWand.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ArrowWand : MonoBehaviour
 {
+    public GameObject spawnPoint;
+    public RaycastHit hit;
+    public GameObject hitObject;
+    public bool hasHit = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,22 @@ public class ArrowWand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawRay(spawnPoint.transform.position, spawnPoint.transform.up, Color.green);
+        RaycastHit tempHit;
+        Ray ray = new Ray(spawnPoint.transform.position, spawnPoint.transform.up);
+        if(Physics.Raycast(ray, out tempHit))
+        {
+            if (tempHit.collider.gameObject.CompareTag("Tower") || tempHit.collider.gameObject.CompareTag("Ground"))
+            {
+                hasHit = true;
+                hit = tempHit;
+                hitObject = hit.collider.gameObject;
+            }
+        }
+        else
+        {
+            hasHit = false;
+            hitObject = null;
+        }
     }
 }
